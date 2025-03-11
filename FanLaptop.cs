fd0d57561f5ec0963a4b2914554200eafd28a6f1
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-11 08:05:27 +0700

    menambahkan class Fanlaptop

---
+++ FanLaptop.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODUL_4
+{
+
+
+    public enum Mode1 { QueitTurbo, QueitBalance, QueitPerformance };
+    public enum kegiatan { fanTurbo, fanBalance, fanPerformance };
+    internal class FanLaptop
+    {
+        private Mode1 state;
+
+        public FanLaptop()
+        {
+            state = Mode1.QueitTurbo;
+            state = Mode1.QueitBalance;
+            state = Mode1.QueitPerformance;
+        }
+
+        public void UbahStateFan(kegiatan opsi)
+        {
+            switch (state)
+            {
+                case Mode1.QueitTurbo:
+                    if (opsi == kegiatan.fanTurbo)
+                    {
+                        Console.WriteLine("Turbo");
+                        state = Mode1.QueitTurbo;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Fan Queit menjadi Turbo");
+                    }
+                    break;
+
+                case Mode1.QueitBalance:
+                    if (opsi == kegiatan.fanBalance)
+                    {
+                        Console.WriteLine("balance");
+                        state = Mode1.QueitBalance;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Fan Queit menjadi balance");
+                    }
+                    break;
+                case Mode1.QueitPerformance:
+                    if (opsi == kegiatan.fanPerformance)
+                    {
+                        Console.WriteLine("performance");
+                        state = Mode1.QueitPerformance;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Fan Queit menjadi Performance");
+                    }
+                    break;
+
+            }
+        }
+
+        public void getFanLaptop()
+        {
+            Console.WriteLine("Status Fan: " + state);
+        }
+
+        internal void ubahStateFan(MODUL_4.kegiatan trigger)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void UbahStateFan(MODUL_4.kegiatan trigger)
+        {
+            throw new NotImplementedException();
+        }
+
+        public class kegiatan
+        {
+            internal static kegiatan fanTurbo;
+            internal static kegiatan fanBalance;
+            internal static kegiatan fanPerformance;
+        }
+    }
+}
+
+
--- Program.cs
+++ Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+using static MODUL_4.FanLaptop;
+using MODUL_4;
+
+namespace MODUL_4
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Produk Elektronik");
+            Console.WriteLine("===================================");
+            Console.WriteLine("Produk Elektronik \t |    Kode Produk");
+            Console.WriteLine("===================================");
+            foreach (kodeProduk.produkElektronik kel in Enum.GetValues(typeof(kodeProduk.produkElektronik)))
+            {
+                String namaProduk = kel.ToString();
+                Console.WriteLine($"{kel,-16} |     {kodeProduk.GetkodeProduk(kel)}");
+            }
+
+            Console.WriteLine("\n===================================");
+            FanLaptop door = new FanLaptop();
+
+            door.getFanLaptop();
+            String input = "";
+            while (input != "exit")
+            {
+                Console.WriteLine("Masukkan Perintah (bukaPintu/kunciPintu)");
+                input = Console.ReadLine();
+                if (Enum.TryParse(input, result: out kegiatan trigger))
+                {
+                    door.UbahStateFan(trigger);
+                }
+                else
+                {
+                    Console.WriteLine("Perintah tidak valid. Coba lagi.");
+                }
+                Console.WriteLine("===================================");
+            }
+        }
+    }
+
+
+
+
+}
